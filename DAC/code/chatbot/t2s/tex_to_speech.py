88c990ab3d0c46abb670c6a7dae9a4b2faa3c0a1
Phan Nhật Linh <93339285+Linhvjc@users.noreply.github.com>
2022-11-25 11:48:44 +0700

    seaborn tutorial

--- DAC/code/chatbot/t2s/tex_to_speech.py
+++ DAC/code/chatbot/t2s/tex_to_speech.py
@@ -1,13 +1,24 @@
 from aspeak import SpeechToSpeakerService, ResultReason
+import time
 
 def say(text = "Nothing to say"):
-    try:
-        speech = SpeechToSpeakerService()
-        result = speech.text_to_speech(text,voice="en-US-JennyNeural", rate="+0%", pitch="+30%", style="cheerful")
-        if result.reason != ResultReason.SynthesizingAudioCompleted:
-            print("Failed to synthesize speech.")
-    except:
-        print("Error occurred while synthesizing speech.")
+    # try:
+    #     speech = SpeechToSpeakerService()
+    #     result = speech.text_to_speech(text,voice="en-US-JennyNeural", rate="+0%", pitch="+30%", style="cheerful")
+    #     if result.reason != ResultReason.SynthesizingAudioCompleted:
+    #         print("Failed to synthesize speech.")
+    # except:
+    #     print("Error occurred while synthesizing speech.")
+    start = time.time()
+    speech = SpeechToSpeakerService()
+    end = time.time()
+    print("Average1: " +str(end - start))
+    start = time.time()
+    speech.text_to_speech(text,voice="en-US-JennyNeural", rate="+0%", pitch="+30%", style="cheerful")
+    end = time.time()
+    print("Average2: " +str(end - start))
+    # if result.reason != ResultReason.SynthesizingAudioCompleted:
+    #     print("Failed to synthesize speech.")
 
 if __name__ == "__main__":
     say("Hello what can I do")
